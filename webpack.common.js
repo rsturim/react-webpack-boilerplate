e02a4b64ac6245ddd858b273479bfb688f739b47
Rich Sturim <rich.sturim@coxautoinc.com>
2018-09-21 11:17:27 -0400

    react upgrade

--- webpack.common.js
+++ webpack.common.js
@@ -1,38 +1,42 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: {
-    main: './src/index.js'
-  },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve('./dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: ['node_modules'],
-        use: [{ loader: 'babel-loader' }],
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader'
-        }, {
-          loader: 'sass-loader'
-        }],
-   	  }
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve('./dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: ['node_modules'],
+                use: [{ loader: 'babel-loader' }]
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: 'index.html'
-    }),
-    new CleanWebpackPlugin(['dist']),
-  ]
-}
+};
